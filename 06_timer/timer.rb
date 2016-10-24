267eb13e8016c0a1bd334e57a194c82bc9837d26
Bill Hefty <billhefty@gmail.com>
2016-10-23 20:55:15 -0500

    Cleaned 06 code

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -6,26 +6,26 @@ class Timer
   end
 
   def time_string
-    res = []
+    result = []
     tt = @seconds.div(3600)
     if tt < 10
       tt = '0' + tt.to_s
     end
 
-    res.push(tt)
+    result.push(tt)
     tt = (@seconds - @seconds.div(3600) * 3600).div(60)
     if tt < 10
       tt = '0' + tt.to_s
     end
 
-    res.push(tt)
-    tt=@seconds-@seconds.div(3600)*3600-((@seconds-@seconds.div(3600)*3600).div(60))*60
+    result.push(tt)
+    tt = @seconds - @seconds.div(3600) * 3600 - ((@seconds-@seconds.div(3600) * 3600).div(60)) * 60
     if tt<10
       tt = '0' + tt.to_s
     end
 
-    res.push(tt)
-    res.join(':')
+    result.push(tt)
+    result.join(':')
   end
-  
+
 end
